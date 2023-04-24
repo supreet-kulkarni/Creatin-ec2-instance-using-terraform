acd236b76f4d5c0da04fa6744a81e765b97cacee
supreet-kulkarni <89987704+supreet-kulkarni@users.noreply.github.com>
2023-04-24 16:08:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web-serverS" {
+  ami           = "ami-062df10d14676e201"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1a"
+  key_name = "TerraKey"
+ tags = {
+    Name = "Webservers"
+  }
+}
+
+
