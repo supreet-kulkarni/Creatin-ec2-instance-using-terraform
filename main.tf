1e9fa664096863578cec507d9c453e1f31671f9c
supreet-kulkarni <89987704+supreet-kulkarni@users.noreply.github.com>
2023-04-25 16:56:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,21 +18,21 @@ resource "aws_instance" "web_server" {
   user_data = <<EOF
                      #!/bin/bash
                      sudo su 
-                    apt update
-                    apt install apache2 -y
-                    wget http://prdownloads.sourceforge.net/awstats/awstats-7.3.tar.gz
-                    tar -xvzf awstats-7.3.tar.gz
-                    cd awstats-7.3/
-                    cd wwwroot/
-                    cp -r icon /var/www/html/
-                    chown www-data:www-data -R /var/www/html/icon/
-                    a2enmod cgi
-                    service apache2 restart
-                    mv cgi-bin awstat
-                    chown www-data:www-data -R awstat
-                    mv awstat /usr/lib/cgi-bin
-                    cd /usr/lib/cgi-bin/awstat
-                    cp awstats.model.conf awstats.linux.conf
+                     apt update
+                     apt install apache2 -y
+                     wget http://prdownloads.sourceforge.net/awstats/awstats-7.3.tar.gz
+                     tar -xvzf awstats-7.3.tar.gz
+                     cd awstats-7.3/
+                     cd wwwroot/
+                     cp -r icon /var/www/html/
+                     chown www-data:www-data -R /var/www/html/icon/
+                     a2enmod cgi
+                     service apache2 restart
+                     mv cgi-bin awstat
+                     chown www-data:www-data -R awstat
+                     mv awstat /usr/lib/cgi-bin
+                     cd /usr/lib/cgi-bin/awstat
+                     cp awstats.model.conf awstats.linux.conf
 
             EOF
  tags = {
