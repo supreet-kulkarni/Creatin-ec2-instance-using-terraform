61c50ff916a7b4f76d4a0818c73414f112c963c0
supreet-kulkarni <89987704+supreet-kulkarni@users.noreply.github.com>
2023-04-25 16:43:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,28 @@ terraform {
 provider "aws" {
     region = "ap-south-1"  
 }
-resource "aws_instance" "web-serverS" {
+resource "aws_instance" "web_server" {
   ami           = "ami-062df10d14676e201"
   instance_type = "t2.micro"
   availability_zone = "ap-south-1a"
   key_name = "TerraKey"
+  user_data = <<EOF
+                     #!/bin/bash
+                     sudo su
+                     curl --location https://rpm.nodesource.com/setup_16.x | bash -
+                     yum -y install nodejs
+                     yum -y install git
+                     git clone https://github.com/supreet-kulkarni/Sample-Node-APP.git
+                     cd /Sample-Node-APP
+                     npm install
+                     npm start
+            EOF
  tags = {
-    Name = "Webservers"
+    Name = "Webserver"
   }
 }
 
+output "instance_ip_address" {
+  value = aws_instance.web_server.public_ip
+}
 
